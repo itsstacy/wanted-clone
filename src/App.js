fb06e5b0e9f70ea1b3f542f51ae0bfb4dcff5297
Seowoo Choi <264826@naver.com>
2022-06-17 21:14:17 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './styles/App.css';
+import "./styles/App.css";
+import Signup from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
